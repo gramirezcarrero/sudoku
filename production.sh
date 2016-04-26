15740fd017d69c8c84f2af2354ebf92e5a3f6cf8
gramirezcarrero <gramirezcarrero@gmail.com>
2016-04-26 09:55:42 -0500

    cambios separado js del html

--- production.sh
+++ production.sh
@@ -1,1 +1,1 @@
-cp -r bower_components/bootstrap/dist public & cp -r index.html public/ && cp css/style.css public/css/ && sudo chmod -R 777 public 
+cp -r bower_components/bootstrap/dist public & cp -r index.html public/ && cp css/style.css public/css/ && cp script.js public/js/ && sudo chmod -R 777 public 
