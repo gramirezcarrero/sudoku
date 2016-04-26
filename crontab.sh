eeb4a16acf3fe8cb4df3c0c23d22565a70aecbe5
gramirezcarrero <gramirezcarrero@gmail.com>
2016-04-26 08:51:53 -0500

    script para hacer el dev. cambios en estilos del sudoku

---
+++ crontab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+/Users/geronimo/sudoku/production.sh
+ sleep 5
+done
---
+++ production.sh
@@ -0,0 +1,1 @@
+cp -r bower_components/bootstrap/dist public & cp -r index.html public/ && cp css/style.css public/css/ && sudo chmod -R 777 public 
