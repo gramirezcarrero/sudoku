50a002c9cc28a3699e9bd201d494c66657d53b48
gramirezcarrero <gramirezcarrero@gmail.com>
2016-11-11 19:50:05 -0500

    cambios en sudoku para automatizar el sudoku

--- production.sh
+++ production.sh
@@ -1,1 +1,4 @@
-cp -r bower_components/bootstrap/dist public & cp -r index.html public/ && cp css/style.css public/css/ && sudo chmod -R 777 public 
+bower install bootstrap
+mkdir public/css
+cp bower_components/bootstrap/dist/css/bootstrap.min.css public/css
+cp -r bower_components/bootstrap/dist public & cp -r index.html public/ && cp css/style.css public/css/ && sudo chmod -R 777 public
